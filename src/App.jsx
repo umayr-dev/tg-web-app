6074024a612928fdc50f1a5a033d8536902a3f12
umayr-dev <abuqodir0107@gmail.com>
2025-03-08 15:01:59 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
-
-import { useState , useEffect } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./index.css";
-
-const testCategories = [
-  { id: 1, name: "Benefit/EMAL", count: 7, special: false, logo: null },
-  { id: 2, name: "Benefit/Бенефит гипсокартон", count: 1, special: false, logo: null },
-  { id: 3, name: "Benefit/Бенефит калаpaнт для колеровичные", count: 16, special: false, logo: null },
-  { id: 4, name: "Benefit/Бенефит сухой смес", count: 1, special: false, logo: null },
-  { id: 5, name: "Benefit/ВД-АК", count: 15, special: false, logo: null },
-  { id: 6, name: "COLORANT SILKCOAT", count: 12, special: false, logo: null },
-  { id: 7, name: "DEMIR PROFIL", count: 33, special: false, logo: "https://files.forlabs.uz/forlabs/15845ed6-a07a-4b53-9112-f83bcf4e6f35" },
-  { id: 8, name: "FUGA VISMUT", count: 11, special: false, logo: null },
-  { id: 9, name: "Mir Instrument", count: 459, special: false, logo: null },
-  { id: 10, name: "OOO. ASMACO", count: 47, special: false, logo: null },
-  { id: 11, name: "OOO.OSCAR.UZ", count: 186, special: false, logo: null },
-  { id: 12, name: "OOO.SILKCOAT", count: 102, special: false, logo: "https://files.forlabs.uz/forlabs/15845ed6-a07a-4b53-9112-f83bcf4e6f35" },
-  { id: 13, name: "SEFFAF PVX TRUBO FITINGLAR", count: 145, special: false, logo: null },
-  { id: 14, name: "Strum", count: 58, special: false, logo: null },
-  { id: 15, name: "VERO SKOCH", count: 34, special: false, logo: null },
-  { id: 16, name: "VERO SKOCH", count: 34, special: false, logo: null },
-  { id: 17, name: "VERO SKOCH", count: 34, special: false, logo: null },
-  { id: 18, name: "VERO SKOCH", count: 34, special: false, logo: null }
-];
 
 const CategoryCard = ({ category, size }) => {
   return (
@@ -41,7 +20,14 @@ const CategoryGrid = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    setCategories(testCategories);
+    // axios.get("")
+    axios.get("https://kroff-api.eduflow.uz/api/v1/categories")
+      .then(response => {
+        setCategories(response.data);
+      })
+      .catch(error => {
+        console.error("Kategoriya yuklashda xatolik:", error);
+      });
   }, []);
 
   const pattern = [
